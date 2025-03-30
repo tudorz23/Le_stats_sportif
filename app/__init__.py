1db780747fec81a50746822dda36aa1da0cc9c4d
Tudor Zaharia <zahariatm@yahoo.com>
2025-03-30 20:21:54 +0300

    Added logging.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 import os
+import time
 from flask import Flask
 from app.data_ingestor import DataIngestor
 from app.task_runner import ThreadPool
+import logging
+import logging.handlers as handle
+from logging import DEBUG
 
 if not os.path.exists('results'):
     os.mkdir('results')
@@ -9,7 +13,28 @@ if not os.path.exists('results'):
 # Create server
 webserver = Flask(__name__)
 
+# Initialize data ingestor
 webserver.data_ingestor = DataIngestor("./nutrition_activity_obesity_usa_subset.csv")
+
+# Initialize logging stuff: logger gets a handler, who gets a formatter
+
+# Set formatter
+formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s')
+formatter.converter = time.gmtime
+
+# Set handler to maximum 65KB
+handler = handle.RotatingFileHandler(filename='webserver.log', maxBytes=65536,
+                                     backupCount=3, encoding='utf-8')
+handler.setLevel(DEBUG)
+handler.setFormatter(formatter)
+
+# Set logger
+webserver.logger = logging.getLogger('webserver_logger')
+webserver.logger.setLevel(DEBUG)
+webserver.logger.addHandler(handler)
+
 webserver.tasks_runner = ThreadPool(webserver.data_ingestor)
+
+webserver.logger.info("Server is on, here we go!")
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -13,12 +13,14 @@ def create_task(data, task_type: d_s.TaskType):
     """
     # Check if the server is shutting down
     if webserver.tasks_runner.is_shutdown.is_set():
+        webserver.logger.error("Cannot create new tasks, server is shutting down!")
         return jsonify( {"status" : "error", "reason" : "shutting down"} )
 
     # Create task and pass it to the threadpool
     if "question" in data:
         question = data["question"]
     else:
+        webserver.logger.error("You should attach a question to your request!")
         return jsonify( {"status": "error", "reason": "where is your question?"} )
 
     state = data["state"] if "state" in data else None
@@ -27,6 +29,7 @@ def create_task(data, task_type: d_s.TaskType):
     task = d_s.Task(job_id, question, state, task_type)
     webserver.tasks_runner.enqueue_task(task)
 
+    webserver.logger.info("Added job {} to the tasks queue.".format(job_id))
     return jsonify( {"job_id": job_id} )
 
 
@@ -34,54 +37,63 @@ def create_task(data, task_type: d_s.TaskType):
 def states_mean_request():
     # Get request data
     data = request.json
+    webserver.logger.info("Received /api/states_mean request.")
     return create_task(data, d_s.TaskType.STATES_MEAN)
 
 @webserver.route('/api/state_mean', methods=['POST'])
 def state_mean_request():
     # Get request data
     data = request.json
+    webserver.logger.info("Received /api/state_mean request.")
     return create_task(data, d_s.TaskType.STATE_MEAN)
 
 @webserver.route('/api/best5', methods=['POST'])
 def best5_request():
     # Get request data
     data = request.json
+    webserver.logger.info("Received /api/best5 request.")
     return create_task(data, d_s.TaskType.BEST5)
 
 @webserver.route('/api/worst5', methods=['POST'])
 def worst5_request():
     # Get request data
     data = request.json
+    webserver.logger.info("Received /api/worst5 request.")
     return create_task(data, d_s.TaskType.WORST5)
 
 @webserver.route('/api/global_mean', methods=['POST'])
 def global_mean_request():
     # Get request data
     data = request.json
+    webserver.logger.info("Received /api/global_mean request.")
     return create_task(data, d_s.TaskType.GLOBAL_MEAN)
 
 @webserver.route('/api/diff_from_mean', methods=['POST'])
 def diff_from_mean_request():
     # Get request data
     data = request.json
+    webserver.logger.info("Received /api/diff_from_mean request.")
     return create_task(data, d_s.TaskType.DIFF_FROM_MEAN)
 
 @webserver.route('/api/state_diff_from_mean', methods=['POST'])
 def state_diff_from_mean_request():
     # Get request data
     data = request.json
+    webserver.logger.info("Received /api/state_diff_from_mean request.")
     return create_task(data, d_s.TaskType.STATE_DIFF_FROM_MEAN)
 
 @webserver.route('/api/mean_by_category', methods=['POST'])
 def mean_by_category_request():
     # Get request data
     data = request.json
+    webserver.logger.info("Received /api/mean_by_category request.")
     return create_task(data, d_s.TaskType.MEAN_BY_CATEGORY)
 
 @webserver.route('/api/state_mean_by_category', methods=['POST'])
 def state_mean_by_category_request():
     # Get request data
     data = request.json
+    webserver.logger.info("Received /api/state_mean_by_category request.")
     return create_task(data, d_s.TaskType.STATE_MEAN_BY_CATEGORY)
 
 @webserver.route('/api/graceful_shutdown', methods=['GET'])
@@ -89,8 +101,12 @@ def graceful_shutdown_request():
     # Check if there are still jobs in the queue.
     still_processing = not webserver.tasks_runner.tasks_queue.empty()
 
+    webserver.logger.info("Received /api/graceful_shutdown request.")
+
     # Notify the threadpool about the shutdown
     webserver.tasks_runner.manage_shutdown()
+
+    webserver.logger.info("Webserver is shutting down.")
 
     if still_processing:
         return jsonify( {"status" : "running"})
@@ -101,6 +117,8 @@ def get_all_jobs_request():
     """"
     Return al job_id's, with their current status (running/done).
     """
+    webserver.logger.info("Received /api/jobs request.")
+
     response = {"status": "done", "data": []}
 
     for i in range(1, webserver.tasks_runner.job_counter):
@@ -114,22 +132,32 @@ def get_num_jobs_request():
     """"
     Return the number of jobs which are currently running
     """
+    webserver.logger.info("Received /api/num_jobs request.")
+
     running_jobs = len(list(filter(
             lambda status: status == "running", webserver.tasks_runner.jobs_status.values())))
+
+    webserver.logger.info("There are {} jobs currently running.".format(running_jobs))
     return jsonify( {"num_jobs" : running_jobs} )
 
 
 @webserver.route('/api/get_results/<job_id>', methods=['GET'])
 def get_response(job_id):
     job_id = int(job_id)
+    webserver.logger.info("Received /api/get_results request for job {}.".format(job_id))
+
     status = webserver.tasks_runner.get_job_status(job_id)
     if status is None:
+        webserver.logger.error("The requested job_id, {}, is invalid!".format(job_id))
         return jsonify( {"status" : "error", "reason" : "Invalid job_id"} )
 
     if status == "running":
+        webserver.logger.info("Job {} is currently running.".format(job_id))
         return jsonify( {"status" : "running"} )
 
     # Here, status == "done"
+    webserver.logger.info("Job {} is done.".format(job_id))
+
     # Load result from file
     target_file = "results/{}.json".format(job_id)
     with open(target_file, "r", encoding="utf-8") as res_file:
